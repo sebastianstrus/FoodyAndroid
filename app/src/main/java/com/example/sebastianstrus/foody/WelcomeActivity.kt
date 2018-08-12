de7d3b1b6572fa98faae3f30441db4003ef5ba7f
Sebastian Strus <sebastian.strus1@gmail.com>
2018-08-12 17:16:53 +0200

    Hide title bar

--- app/src/main/java/com/example/sebastianstrus/foody/WelcomeActivity.kt
+++ app/src/main/java/com/example/sebastianstrus/foody/WelcomeActivity.kt
@@ -1,15 +1,23 @@
 package com.example.sebastianstrus.foody
 
 import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.foody_background)
+        videoview_welcome.setVideoURI(uri)
+        videoview_welcome.start()
 
 
         login_button_welcome.setOnClickListener {
@@ -22,4 +30,5 @@ class WelcomeActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
 }
